6b610fb78f038a0677a11a5634db6338b05e08a9
MattQ <scarcella.matteo@icloud.com>
2025-08-13 18:49:19 +0200

    fix: Update Vite configuration for improved build input structure and clarity

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  root: './',
+  base: '/', // Assicura che questo sia impostato sulla root
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Directory di output per la build
     rollupOptions: {
-      input: 'index.html'
+      input: {
+        main: './index.html' // Specifica il punto di ingresso principale
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src' // Opzionale: Aggiunge un alias per la directory source
     }
   }
 })
