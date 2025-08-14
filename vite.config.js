cecd59e67a3f87c3a4531aa4bd3d0d1917c8c55c
MattQ <scarcella.matteo@icloud.com>
2025-08-14 17:03:44 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Assicura che questo sia impostato sulla root
-  build: {
-    outDir: 'dist', // Directory di output per la build
-    rollupOptions: {
-      input: {
-        main: './index.html' // Specifica il punto di ingresso principale
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src' // Opzionale: Aggiunge un alias per la directory source
-    }
-  }
+  base: '/',
 })
