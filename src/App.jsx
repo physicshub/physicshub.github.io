b80f25baa4fe1d5fce1c9a2749019ad8c36233b1
MattQ <scarcella.matteo@icloud.com>
2025-10-02 22:48:01 +0200

    refactor: update routing to use HashRouter and enhance Error page with visual elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom"
-import { Home } from "./pages/home.jsx"
-import { Error } from "./pages/error.jsx"
+import { Route, HashRouter, Routes } from "react-router-dom";
+import { Home } from "./pages/home.jsx";
+import { Error } from "./pages/error.jsx";
 import "katex/dist/katex.min.css";
 
-import { BouncingBall } from "./pages/simulations/BouncingBall.jsx"
-import { VectorsOperations } from "./pages/simulations/VectorsOperations.jsx"
-import { BallAcceleration } from "./pages/simulations/BallAcceleration.jsx"
-import { BallGravity } from "./pages/simulations/BallGravity.jsx"
-import { SpringConnection } from "./pages/simulations/SpringConnection.jsx"
-import { SimplePendulum } from "./pages/simulations/SimplePendulum.jsx"
-import Contribute from "./pages/contribute.jsx"
-import About from "./pages/about.jsx"
-import Simulations from "./pages/simulations.jsx"
+import { BouncingBall } from "./pages/simulations/BouncingBall.jsx";
+import { VectorsOperations } from "./pages/simulations/VectorsOperations.jsx";
+import { BallAcceleration } from "./pages/simulations/BallAcceleration.jsx";
+import { BallGravity } from "./pages/simulations/BallGravity.jsx";
+import { SpringConnection } from "./pages/simulations/SpringConnection.jsx";
+import { SimplePendulum } from "./pages/simulations/SimplePendulum.jsx";
+import Contribute from "./pages/contribute.jsx";
+import About from "./pages/about.jsx";
+import Simulations from "./pages/simulations.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-          <Route index element={<Home />} />
-          <Route path="/contribute" element={<Contribute />} />
-          <Route path="/simulations" element={<Simulations />} />
-          <Route path="/about" element={<About />} />
-          <Route path="BouncingBall" element={<BouncingBall />} />
-          <Route path="VectorsOperations" element={<VectorsOperations />} />
-          <Route path="BallAcceleration" element={<BallAcceleration />} />
-          <Route path="BallGravity" element={<BallGravity />} />
-          <Route path="SpringConnection" element={<SpringConnection />} />
-          <Route path="SimplePendulum" element={<SimplePendulum />} />
-          <Route path="*" element={<Error />} />
+        <Route index element={<Home />} />
+        <Route path="/contribute" element={<Contribute />} />
+        <Route path="/simulations" element={<Simulations />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/BouncingBall" element={<BouncingBall />} />
+        <Route path="/VectorsOperations" element={<VectorsOperations />} />
+        <Route path="/BallAcceleration" element={<BallAcceleration />} />
+        <Route path="/BallGravity" element={<BallGravity />} />
+        <Route path="/SpringConnection" element={<SpringConnection />} />
+        <Route path="/SimplePendulum" element={<SimplePendulum />} />
+        <Route path="*" element={<Error />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,11 +1,15 @@
 import Footer from "../components/Footer.jsx"
 import Button from "../components/Button.jsx"
 import Header from "../components/Header.jsx"
+import Stars from "../components/Stars.jsx";
+import GradientBackground from "../components/GradientBackground.jsx";
 
 export function Error() {
     return(
         <>
             <Header/>
+            <Stars color="#AEE3FF" opacity={0.4} starDensity={0.005}/>
+            <GradientBackground/>
             <div className="page-404">
                 <h1>404</h1>
                 <p>Oops! The page you are looking for does not exist. Please return to the main page and try again.</p>
