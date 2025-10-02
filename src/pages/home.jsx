3f01a9179fb8dd36f61d286591440bce540460da
MattQ <scarcella.matteo@icloud.com>
2025-10-02 21:30:29 +0200

    refactor: remove Google Translate component import from Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import FullLandingPage from "../components/FullLandingPage.jsx";
-import GoogleTranslate from '../components/GoogleTranslator.jsx'; // Disabled due to issues
 
 export function Home() {
   return (
