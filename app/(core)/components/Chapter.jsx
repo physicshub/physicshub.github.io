81814aaaa5a3606985a145b76b40a0b258c87728
Meet Shah <shahmeet8210@gmail.com>
2025-12-14 18:16:16 -0500

    Ehnance Simulations Page #100

--- app/(core)/components/Chapter.jsx
+++ app/(core)/components/Chapter.jsx
@@ -1,26 +1,48 @@
 // app/components/Chapter.jsx
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import Tag from "./Tag.jsx"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Tag from "./Tag.jsx";
 import Link from 'next/link.js';
 
 function Chapter(props) {
-    return(
+    return (
         <section id={props.id} className="chapter-card">
             <div className='chapter-card-overlay'>
+
+                {/* Tags */}
                 <div className="chapter-card-tags-container">
                     {props.tags.map((tag, idx) => (
                         <Tag tag={tag} key={tag.id || idx} />
                     ))}
                 </div>
-                <h2 className='text-2xl'>{props.isABlog ? `#${props.id} ${props.name}` : `Chapter ${props.id}: ${props.name}`}</h2>
+
+                {/* Title with optional icon */}
+                <h2 className='text-2xl flex items-center gap-2'>
+                    {props.isABlog 
+                        ? `#${props.id} ${props.name}` 
+                        : `Chapter ${props.id}: ${props.name}`}
+                    {props.icon && (
+                        <img
+                            src={props.icon} 
+                            alt={`${props.name} icon`} 
+                            width={24} 
+                            height={24} 
+                            className="chapter-icon"
+                        />
+                    )}
+                </h2>
+
+                {/* Description */}
                 <p>{props.desc}</p>
-                <Link href={props.link}>{props.isABlog ? "Go to blog" :  "Go to simulation"}
+
+                {/* Link */}
+                <Link href={props.link}>
+                    {props.isABlog ? "Go to blog" : "Go to simulation"}
                     <FontAwesomeIcon icon={faArrowRight} style={{ marginLeft: '10px' }}/>
                 </Link>
             </div>
-        </section> 
+        </section>
     );
 }
 
-export default Chapter
+export default Chapter;
